866d45ab8e49dbe383163822097ca5095dfa6779
geryheselmans <gery.heselmans@hotmail.com>
2017-02-19 17:55:03 +0100

    Implemented new, save, find and delete methods, also defined a repository interface to delegate them to

--- model/author.go
+++ model/author.go
@@ -6,3 +6,37 @@ type Author struct {
 	lastName   string
 	mail       string
 }
+
+type AuthorRepository interface {
+	Save(author *Author) error
+	FindByAuthorName(authorName string) (author *Author, err error)
+	Delete(authorName string) error
+	Clear() error
+}
+
+func NewAuthor(authorName string, firstName string, lastName string, mail string) *Author {
+	return &Author{authorName, firstName, lastName, mail}
+}
+
+func (author *Author) FindByAuthorName(authorStorage AuthorRepository, authorName string) error {
+	foundAuthor, err := authorStorage.FindByAuthorName(authorName)
+
+	if err == nil {
+		return err
+	}
+
+	author.authorName = foundAuthor.authorName
+	author.firstName = foundAuthor.firstName
+	author.lastName = foundAuthor.lastName
+	author.mail = author.mail
+
+	return nil
+}
+
+func (author *Author) Save(authorStorage AuthorRepository) error {
+	return authorStorage.Save(author)
+}
+
+func (author *Author) Delete(authorStorage AuthorRepository, authorName string) error {
+	return authorStorage.Delete(authorName)
+}
