3b2a52fd2cb66caa1b86beb9c6fa2ca9290ab43f
geryheselmans <gery.heselmans@hotmail.com>
2017-02-19 16:39:43 +0100

    Methods created but implemented with fake responses

--- web/api/v1/author.go
+++ web/api/v1/author.go
@@ -14,7 +14,57 @@ func NewAuthorAPI() *AuthorAPI {
 }
 
 func (api *AuthorAPI) Register(router *mux.Router) {
-	router.HandleFunc("/author/hello", func(response http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(response, "Hello world")
-	})
+	router.HandleFunc("/authors", GetAllAuthors).
+		Methods("GET").
+		HeadersRegexp("Accept", "application/(xml|json)")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}", GetAuthorById).
+		Methods("GET").
+		HeadersRegexp("Accept", "application/(xml|json)")
+
+	router.HandleFunc("/authors", CreateAuthor).
+		Methods("POST").
+		HeadersRegexp("Content-Type", "application/(xml|json)")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}", UpdateAuthorById).
+		Methods("PUT").
+		HeadersRegexp("Content-Type", "application/(xml|json)")
+
+	router.HandleFunc("/authors", DeleteAllAuthors).
+		Methods("DELETE")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}", DeleteAuthorById).
+		Methods("DELETE")
+}
+
+func GetAllAuthors(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+	response.Header().Add("Content-Type", "application/json")
+
+	fmt.Fprint(response, "{\"test\":\"Hello world\"}")
+}
+
+func GetAuthorById(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+	response.Header().Add("Content-Type", "application/json")
+
+	vars := mux.Vars(request)
+
+	fmt.Fprintf(response, "{\"test\":\"%s\"}", vars["authorId"])
+}
+
+func CreateAuthor(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusCreated)
+}
+
+func UpdateAuthorById(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+}
+
+func DeleteAllAuthors(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+}
+
+func DeleteAuthorById(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
 }
--- web/api/v1/document.go
+++ web/api/v1/document.go
@@ -14,7 +14,57 @@ func NewDocumentApi() *DocumentApi {
 }
 
 func (api *DocumentApi) Register(router *mux.Router) {
-	router.HandleFunc("/document/hello", func(response http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(response, "Hello world")
-	})
+	router.HandleFunc("/authors/{authorId:[0-9]+}/documents", GetAllDocumentsByAuthor).
+		Methods("GET").
+		HeadersRegexp("Accept", "application/(xml|json)")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}/documents/{documentId:[0-9]+}", GetDocumentByIdAndAuthor).
+		Methods("GET").
+		HeadersRegexp("Accept", "application/(xml|json)")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}/documents", CreateDocumentByAuthor).
+		Methods("POST").
+		HeadersRegexp("Content-Type", "application/(xml|json)")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}/documents/{documentId:[0-9]+}", UpdateDocumentByAuthor).
+		Methods("PUT").
+		HeadersRegexp("Content-Type", "application/(xml|json)")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}/documents", DeleteAllDocumentsByAuthor).
+		Methods("DELETE")
+
+	router.HandleFunc("/authors/{authorId:[0-9]+}/documents/{id:[0-9]+}", DeleteDocumentByAuthorAndId).
+		Methods("DELETE")
+}
+
+func GetAllDocumentsByAuthor(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+	response.Header().Add("Content-Type", "application/json")
+
+	fmt.Fprint(response, "{\"test\":\"Hello world\"}")
+}
+
+func GetDocumentByIdAndAuthor(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+	response.Header().Add("Content-Type", "application/json")
+
+	vars := mux.Vars(request)
+
+	fmt.Fprintf(response, "{\"test\":\"%s\"}", vars["documentId"])
+}
+
+func UpdateDocumentByAuthor(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusCreated)
+}
+
+func CreateDocumentByAuthor(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+}
+
+func DeleteDocumentByAuthorAndId(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
+}
+
+func DeleteAllDocumentsByAuthor(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(http.StatusOK)
 }
