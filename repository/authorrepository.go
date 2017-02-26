28668f086880c4412fd6aaa1a7a671b37e7df63f
geryheselmans <gery.heselmans@hotmail.com>
2017-02-26 08:56:24 +0100

    In memory repo is now thread safe

--- repository/authorrepository.go
+++ repository/authorrepository.go
@@ -7,7 +7,7 @@ import (
 
 type InMemoryAuthorRepository struct {
 	storage map[string]*model.Author
-	lock    sync.RWMutex
+	mu      sync.RWMutex
 }
 
 func NewInMemoryAuthorRepository() InMemoryAuthorRepository {
@@ -17,12 +17,18 @@ func NewInMemoryAuthorRepository() InMemoryAuthorRepository {
 }
 
 func (repo InMemoryAuthorRepository) Save(authorName string, author *model.Author) error {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
 	repo.storage[authorName] = author
 
 	return nil
 }
 
 func (repo InMemoryAuthorRepository) FindAll() (authors []*model.Author, err error) {
+	repo.mu.RLock()
+	defer repo.mu.RUnlock()
+
 	authorList := make([]*model.Author, len(repo.storage))
 	i := 0
 	for _, value := range repo.storage {
@@ -34,16 +40,25 @@ func (repo InMemoryAuthorRepository) FindAll() (authors []*model.Author, err err
 }
 
 func (repo InMemoryAuthorRepository) FindByAuthorName(authorName string) (author *model.Author, err error) {
+	repo.mu.RLock()
+	defer repo.mu.RUnlock()
+
 	return repo.storage[authorName], nil
 }
 
 func (repo InMemoryAuthorRepository) Delete(authorName string) error {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
 	delete(repo.storage, authorName)
 
 	return nil
 }
 
 func (repo InMemoryAuthorRepository) Clear() error {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
 	for key := range repo.storage {
 		delete(repo.storage, key)
 	}
