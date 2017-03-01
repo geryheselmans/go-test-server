bc19410091532e686ef723c969af9ec38a8cd26d
geryheselmans <gery.heselmans@hotmail.com>
2017-03-01 21:59:12 +0100

    First logging using logrus

--- cmd/testserver/main.go
+++ cmd/testserver/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"github.com/geryheselmans/go-test-server/repository"
 	"github.com/geryheselmans/go-test-server/web"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	inMemoryAuthorRepository := repository.NewInMemoryAuthorRepository()
+
+	log.Info("Starting service")
 
 	h := web.New(inMemoryAuthorRepository)
 
